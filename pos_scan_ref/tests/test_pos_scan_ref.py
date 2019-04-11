f11e99b944a3ac52131a9ef2e757575342aac971
ommo73 <rafailov@it-projects.info>
2019-04-11 18:35:38 +0500

    :shield: pos_scan_ref updated tests. cover the basic functionality

--- pos_scan_ref/tests/test_pos_scan_ref.py
+++ pos_scan_ref/tests/test_pos_scan_ref.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2019 Artem Rafailov <https://it-projects.info/team/Ommo73/>
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl.html).
 import odoo.tests
 
 
@@ -7,6 +9,13 @@ import odoo.tests
 class TestUi(odoo.tests.HttpCase):
 
     def test_pos_scan_ref(self):
+        env = self.env
+        env['ir.module.module'].search([('name', '=', 'pos_scan_ref')], limit=1).state = 'installed'
+
+        env['product.template'].search([('name', '=', 'Miscellaneous')]).write({
+            'default_code': '1234567890333',
+        })
+
         # without a delay there might be problems on the steps whilst opening a POS
         # caused by a not yet loaded button's action
         self.phantom_js("/web",
